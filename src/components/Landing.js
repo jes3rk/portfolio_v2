a9d23a23382e15814b009a77fb954de685e4c056
Joseph Schefer <joseph.schefer@gmail.com>
2018-01-25 12:47:41 -0500

    preliminary title-changer

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,12 +3,34 @@ import Row from './primary-components/Row';
 import Column from './primary-components/Column';
 
 class Landing extends Component {
+  state = {
+    titles: ["Full Stack Developer", "Programmer", "Composer", "Technical Analyst"],
+    currentTitle: "Full Stack Developer"
+  };
+
+
+  componentDidMount() {
+    const titleChanger = () => {
+      setTimeout(() => {
+        let randTitle = this.state.titles[Math.floor(Math.random() * this.state.titles.length)];
+        this.setState({ currentTitle: randTitle });
+        titleChanger();
+      }, 2000);
+    };
+    titleChanger();
+  };
+
   render() {
     return (
       <div className="container">
-        <Row>
+        <Row className='center-align'>
           <Column className='align-center' size="12">
-            <h1>Hello World</h1>
+            <h1>Joseph Schefer</h1>
+          </Column>
+        </Row>
+        <Row className='center-align'>
+          <Column size="12">
+            <h2>I am a {this.state.currentTitle}</h2>
           </Column>
         </Row>
       </div>
